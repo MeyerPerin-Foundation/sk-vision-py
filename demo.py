2ba3d1f1ca06b77ceb8d761c3b6ac8620d210da7
Lucas Meyer <lucas.meyer@microsoft.com>
2023-12-26 16:17:54 -0800

    Added meme detector

--- demo.py
+++ demo.py
@@ -9,8 +9,8 @@ from VisionPlugin import Vision
 async def main():
     kernel = sk.Kernel()
     api_key, org_id = sk.openai_settings_from_dot_env()
-    gpt4 = OpenAIChatCompletion("gpt-4", api_key, org_id)
-    kernel.add_chat_service("gpt4", gpt4)
+    gpt3 = OpenAIChatCompletion("gpt-3.5-turbo", api_key, org_id)
+    kernel.add_chat_service("gpt3", gpt3)
 
     vision = kernel.import_skill(Vision())
     plugins = kernel.import_semantic_skill_from_directory(".", "plugins")
@@ -20,12 +20,25 @@ async def main():
         lines = f.readlines()
     
     # remove whitespace characters like `\n` at the end of each line
-    animal_url_list = [x.strip() for x in lines]        
+    animal_url_list = [x.strip() for x in lines]  
 
+    # Interpreting memes
+    meme_base_url = "https://raw.githubusercontent.com/lucas-a-meyer/lucas-a-meyer.github.io/main/images/"
+    meme_url_list = ["meme1.jpg", "meme2.png", "meme3.jpg"] 
+
+    for url in meme_url_list:
+        variables = sk.ContextVariables()
+        variables['prompt'] = "Why is this meme funny?"
+        variables['url'] = meme_base_url + url
+        print(f"Processing meme {meme_base_url + url}")
+        meme = await kernel.run_async(vision['ApplyPromptToImage'], input_vars=variables)
+        print(f"{meme}\n\n")     
+
+    # Generating facts about animals
     count = 0
     # List of animal positions to process so you can easily skip
     # some and save money on OpenAI credits
-    animal_positions_to_process = [2, 9]
+    animal_positions_to_process = [2, 9, 10]
     for url in animal_url_list:
         count += 1
         if count in animal_positions_to_process:
